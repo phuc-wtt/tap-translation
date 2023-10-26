a9db06053b41398751489bfb60c32a3b9adf50ee
phuc.nguyen <phuc.nguyen@webtretho.com>
2023-10-26 17:10:41 +0700

    tap-translation

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const fs = require('fs'); 
 const parser = require('./parser.js').parse;
 
-const csvData=[]
-const trimSpaces = string => string.replace(/(^\s+)|(\s+$)/g, '')
+const csvData = []
+
+//  input file name: translation-input.csv
+//  place translation-input.csv beside translations.js
+//  translations.js content object should have a placeholder for later replace
+//  read input |-> loop through langsObj.en then snr js files within folder
+//             |-> parse into obj and print out to translations.js placeholder 
+//  rm left over
 
 fs.createReadStream('./translation-input.csv')
   .pipe(parser({delimiter: ','}))
@@ -16,12 +22,11 @@ fs.createReadStream('./translation-input.csv')
 })
 
 const writeResult = csvData => {
-
   const langsObj = convertArrToObj(getLangArr(csvData))
   const contentKeyRange = csvData[0].length - Object.keys(langsObj).length
   mapDataToLangObj(csvData, langsObj, contentKeyRange)
 
-  var file = fs.createWriteStream('result.json');
+  const file = fs.createWriteStream('result.json');
   file.on('error', function(err) { console.log('Something wrong!') });
   file.write(JSON.stringify(langsObj,null, 2))
   file.end();
@@ -36,10 +41,6 @@ const checkIsEmptyRow = (rowArr, currentIndex) => {
   }
 }
 
-const getLangArr = arr => arr.shift().filter(item => item != '')
-
-const convertArrToObj = arr => arr.reduce((a, v) => ({ ...a, [v]: {}}), {})
-
 const mapDataToLangObj = (csvData, langsObj, contentKeyRange) => {
   csvData.forEach(row => {
     const contentKey = row.slice(0,contentKeyRange).filter(x=>x).join('.').replace(/\s+/g,'_').toLowerCase()
@@ -48,3 +49,7 @@ const mapDataToLangObj = (csvData, langsObj, contentKeyRange) => {
     })
   })
 }
+
+const getLangArr = arr => arr.shift().filter(item => item != '')
+const convertArrToObj = arr => arr.reduce((a, v) => ({ ...a, [v]: {}}), {})
+const trimSpaces = string => string.replace(/(^\s+)|(\s+$)/g, '')
